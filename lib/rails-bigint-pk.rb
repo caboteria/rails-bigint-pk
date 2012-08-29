5ece8e6971b071715cdb4b2cdc0a53dd2869e2fd
David J. Hamilton <davidjh@hjdivad.com>
2012-08-28 18:23:47 -0700

    Root

---
+++ lib/rails-bigint-pk.rb
@@ -0,0 +1,9 @@
+require "rails-bigint-pk/version"
+
+module Rails
+  module Bigint
+    module Pk
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails-bigint-pk/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Bigint
+    module Pk
+      VERSION = "0.0.1"
+    end
+  end
+end
