c2fac99aa20c2affa90c601a1dac5c393959d06f
Buckley Robinson <brobinson@host-155-6-0-10.sfoffice.verticalresponse.com>
2012-11-13 16:23:23 -0800

    Do not migrate non-existant tables.
    [Fixes #2]

--- lib/generators/bigint_pk/templates/migration.rb
+++ lib/generators/bigint_pk/templates/migration.rb
@@ -2,12 +2,16 @@ class ChangeKeysToBigint < ActiveRecord::Migration
   def change
     Rails.application.eager_load!
     ActiveRecord::Base.subclasses.each do |type|
-      BigintPk.update_primary_key type.table_name, type.primary_key
+      if connection.table_exists? type.table_name
+        BigintPk.update_primary_key type.table_name, type.primary_key
+      end
 
       type.reflect_on_all_associations.select do |association|
         association.macro == :belongs_to
       end.each do |belongs_to_association|
-        BigintPk.update_foreign_key type.table_name, belongs_to_association.foreign_key
+        if connection.table_exists? type.table_name
+          BigintPk.update_foreign_key type.table_name, belongs_to_association.foreign_key
+        end
       end
     end
   end
